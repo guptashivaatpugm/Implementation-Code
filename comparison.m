752869aed53279f9ae51f201c21069816851e63f
guptashivaatpugm <gupta.shivaatpugm@gmail.com>
2025-10-22 14:40:15 +0530

    Update comparison.m

--- comparison.m
+++ comparison.m
@@ -1,7 +1,7 @@
-% Define method names
+
 methods = {'DSA', 'TLBO', 'TDF-IMC', 'IMC-ADRC', 'IMC', 'IMC-PID', 'Proposed'};
 
-% Define performance metric values
+
 IAE  = [4.266e107, 7.238e145, 5.38e144, 9.36e6, 72.54468, 72.37669, 2.29653];
 ITAE = [2.1074e110, 3.5731e148, 2.657e147, 4.20e6, 16817.89, 1697.08, 9.03];
 ISE  = [2.252e214, 5.5683e290, 3.38e288, 1.22e6, 13.99757, 14.79607, 0.9983];
@@ -21,3 +21,4 @@ for i = 1:4
     ylabel(ylabels{i}, 'FontName', 'Times New Roman', 'FontSize', 14, 'FontWeight', 'bold');
     xtickangle(45);
 end
+
